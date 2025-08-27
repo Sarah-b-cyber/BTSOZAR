1da16da4960bb970e2f9e16de41c6f27fde13cef
Rephael <rephael@example.com>
2025-08-27 17:47:34 +0200

    Push complet projet Laravel BTSOZAR avec vendor, node_modules et .env

---
+++ bts/database/migrations/2025_08_27_150535_add_role_and_is_active_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('user');       // rôle par défaut 'user'
+            $table->boolean('is_active')->default(true);  // actif par défaut
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['role', 'is_active']);    // supprime les colonnes si rollback
+        });
+    }
+};
